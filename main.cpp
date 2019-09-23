c97e15c35a774c581e0ba301bafa59df6ffe232b
markomih10 <mmihajlovic@csuchico.edu>
2019-09-23 16:43:16 -0700

    First Last Name Horoscope

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//Authors:
+//Authors:Marko Mihajlovic
 #include<iostream>
 #include<string>
 
@@ -7,9 +7,14 @@ using namespace std;
 int main()
 {
   //declare variables
-
+  string first = "";
+  string last = "";
   //get user input
-
+  cout<<"What is your first name?\n";
+  cin>>first;
+  cout<<"What is your last name?\n";
+  cin>>last;
+  cout<<"Welcome," <<first[0]<<", "<<last[0]<<" .,here is your fortune...";
   //tell fortune
 
   return 0;

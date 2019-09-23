c50cb27f80dfff198ebdbc344d61ab0839075ccf
markomih10 <mmihajlovic@csuchico.edu>
2019-09-23 16:50:01 -0700

    First Last Fortune results

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ int main()
   //declare variables
   string first = "";
   string last = "";
+  int lucky = 0;
   //get user input
   cout<<"What is your first name?\n";
   cin>>first;
@@ -16,6 +17,6 @@ int main()
   cin>>last;
   cout<<"Welcome," <<first[0]<<", "<<last[0]<<" .,here is your fortune...";
   //tell fortune
-
+  cout<<"Your lucky number is "<<first.length()<<".";
   return 0;
 }

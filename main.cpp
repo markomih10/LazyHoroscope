11c2f6c66f2de4a9a036aa6603c937fd2c3ef4be
markomih10 <mmihajlovic@csuchico.edu>
2019-09-25 20:08:51 -0700

    .cpp

--- main.cpp
+++ main.cpp
@@ -6,17 +6,35 @@ using namespace std;
 
 int main()
 {
-  //declare variables
-  string first = "";
-  string last = "";
-  int lucky = 0;
-  //get user input
-  cout<<"What is your first name?\n";
-  cin>>first;
-  cout<<"What is your last name?\n";
-  cin>>last;
-  cout<<"Welcome," <<first[0]<<", "<<last[0]<<" .,here is your fortune...";
-  //tell fortune
-  cout<<"Your lucky number is "<<first.length()<<".";
-  return 0;
+        //declare variables
+        string first = "";
+        string last = "";
+        int index;
+        string constant;
+
+        int lucky = 0;
+        //get user input
+        cout<<"What is your first name?\n";
+        cin>>first;
+        cout<<"What is your last name?\n";
+        cin>>last;
+        cout<<"Welcome, " <<first[0]<<"."<<last[0]<<"., here is your fortune...\n";
+        //tell fortune
+        cout<<"your lucky number is "<<first.length()<<endl;
+
+        if(first[0]=='a' || first[0]=='e' || first[0]=='i' || first[0]=='o' || first[0]=='u' || first[0]=='A' || first[0]=='E' || first[0]=='I' || first[0]=='O' || first[0]=='U')
+        {        
+                cout<<"you are destined to be famous!\n";
+        }
+        else {
+                cout<<"you should keep a low profile."<<endl;
+        }
+         
+                cout<<"have a good day! "<<endl;
+
+
+        
+
+        
+             return 0;
 }
